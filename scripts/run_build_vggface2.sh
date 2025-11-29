f6975408d322208ed3668e7f895501d51fb6ea32
Alireza Ghadimi <arghadimi.bme@gmail.com>
2025-11-29 18:05:56 -0500

    fix sh again

--- scripts/run_build_vggface2.sh
+++ scripts/run_build_vggface2.sh
@@ -22,7 +22,10 @@ cd "$SLURM_SUBMIT_DIR"
 
 python scripts/build_vggface2.py \                     
   --dataset logasja/vggface2 \
-  --split train --out_dir data_vggface2 \
-  --streaming --sample_prob 0.9 --max_samples 50 \
+  --split train \
+  --out_dir data_vggface2 \
+  --streaming \
+  --sample_prob 0.9 \
+  --max_samples 50 \
   --csv_path data_vggface2_first_row.csv \
   --npy_path data_vggface2_landmarks.npy
